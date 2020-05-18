63566af61b757e91da6e456be6ffba66c527e9ed
philipp <philipp@Philipps-MacBook.local>
2020-05-18 22:44:16 +0200

    initial test commit with Azure depdendencies

--- src/main/java/pacApp/UserServiceApplication.java
+++ src/main/java/pacApp/UserServiceApplication.java
@@ -2,13 +2,15 @@ package pacApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+//@SpringBootApplication
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringCloudApplication
 public class UserServiceApplication {
 
 	@Bean
